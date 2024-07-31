d68f09fe712d5259dcdd8f94d83d6e05505e630e
Nosihle Mthembu <bongekam16@gmail.com>
2024-07-31 11:58:33 +0200

    require

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import MyCard from './assets/components/mycardexample'
+import MyCard from './components/mycardexample'
 import './App.css'
 
 function App() {
--- src/components/mycardexample.jsx
+++ src/components/mycardexample.jsx
@@ -1,35 +1,38 @@
-import { LuHeart } from "react-icons/lu";
+// import { LuHeart } from "react-icons/lu";
+import { GiHearts } from "react-icons/gi";
 import { useState } from "react";
 
 function MyCard(){
-    const [color, setColor] = useState("");
+    const [color, setColor] = useState();
     return(
         <div className='container'>
             {/* first component */}
             <div className='housePicture'>
-            <div >{color}<LuHeart type="button"
-        onClick={() => setColor("pink")} style={{
-                width:30,
-                height:30,
-                color:"whitesmoke",
-                float:"right",
-                margin:20,
-            }}/></div>
+                <div className="hearticon">{color}<GiHearts type="button"
+            onClick={() => setColor("pink")} style={{
+                    width:30,
+                    height:30,
+                    color:"whitesmoke",
+                    float:"right",
+                    margin:20,
+                }}/>
+                </div>
             </div>
 
             <div className='details'> {/*second Container*/}
 
                 <div className="secondComponent">  {/*second component*/}
                     <div className='houseName'>
-                    <div className='house'></div>
-                    <div className='years'></div>
+                        <div className='house'>DETACHED HOUSE</div>
+                        <div className='years'><li>5Y OLD</li></div>
                     </div>
                     
-                    <div></div>
-                    <div></div>
+                    <div className="CostPrice">$750,000</div>
+                    <div>742 Evergreen Terrace</div>
                 </div>
+                <hr/>
 
-                     {/*thirth Container*/}
+                {/*thirth Container*/}
                 <div className='rooms'>
                     <div><span></span> </div>
                     <div><span></span> </div>
