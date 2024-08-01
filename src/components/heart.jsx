36c9a9682e52cada81765446a5ed5e36c890a3fb
Nosihle Mthembu <bongekam16@gmail.com>
2024-08-01 09:31:45 +0200

    final changes

--- src/components/heart.jsx
+++ src/components/heart.jsx
@@ -3,29 +3,35 @@ import { LuHeart } from "react-icons/lu";
 import { useState } from "react";
 
 function Heart(){
+    // localStorage.setItem("myCat", "Tom");
     const [isLiked, setIsLiked] = useState(false);
     let iconColour = "red";
+    let filler = "red"
 
     function ChangeLike(){
-      
+        setIsLiked(!isLiked)
+       }
         if(isLiked){
             console.log("liked")
-            setIsLiked(false);
+            // setIsLiked(false);
             iconColour = "red";
+            filler = "red"
         }
 
         if(!isLiked){
             console.log("unliked")
-            setIsLiked(true);
+            // setIsLiked(true);
             iconColour = "white";
+            filler = "none"
         }
-    }
+   
 
     return(
         <div className="hearticon"><LuHeart onClick={ChangeLike} style={{
             width:30,
             height:30,
             color: iconColour,
+            fill: filler,
             float:"right",
             margin:20,
         }}/>
